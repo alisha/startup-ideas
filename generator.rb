c53ecb439bc1939b1fbc187d6c416ed0146055ce
Alisha Ukani <alishaaukani@gmail.com>
2015-03-28 14:06:33 -0400

    Get list of startups

---
+++ generator.rb
@@ -0,0 +1,10 @@
+require 'mechanize'
+
+startups = []
+
+## get list of startups
+mechanize = Mechanize.new
+startuppage = mechanize.get('http://www.startupranking.com/top/united-states')
+startuppage.search('div.name').each do |startup|
+  startups.push(startup.text)
+end
