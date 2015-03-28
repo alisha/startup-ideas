35e50f7a956f6a67e70609d39d057b38d8c7f686
Alisha Ukani <alishaaukani@gmail.com>
2015-03-28 15:37:00 -0400

    finish getting nouns

--- nounFinder.rb
+++ nounFinder.rb
@@ -1,4 +1,19 @@
 require 'mechanize'
+require 'active_support/inflector'
 
-nouns = []
+pages = ['https://www.englishclub.com/vocabulary/nouns-uncountable-list-ab.htm', 'https://www.englishclub.com/vocabulary/nouns-uncountable-list-cd.htm', 'https://www.englishclub.com/vocabulary/nouns-uncountable-list-ef.htm', 'https://www.englishclub.com/vocabulary/nouns-uncountable-list-gh.htm', 'https://www.englishclub.com/vocabulary/nouns-uncountable-list-ij.htm', 'https://www.englishclub.com/vocabulary/nouns-uncountable-list-kl.htm', 'https://www.englishclub.com/vocabulary/nouns-uncountable-list-mn.htm', 'https://www.englishclub.com/vocabulary/nouns-uncountable-list-op.htm', 'https://www.englishclub.com/vocabulary/nouns-uncountable-list-qr.htm', 'https://www.englishclub.com/vocabulary/nouns-uncountable-list-s.htm', 'https://www.englishclub.com/vocabulary/nouns-uncountable-list-tuv.htm', 'https://www.englishclub.com/vocabulary/nouns-uncountable-list-wyz.htm']
 
+## get list of words
+mechanize = Mechanize.new
+$nouns = []
+
+pages.each do |link|
+  page = mechanize.get(link)
+
+  words = page.search('strong').drop(1)
+
+  # strip HTML tags
+  words.each do |word|
+    $nouns.push((word.text.sub(/<\/?strong>/, '')).pluralize)
+  end
+end
